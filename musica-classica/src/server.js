c9abb69289d12bcf621be6d63c06ace2a4026998
dyoca <dyocelin.pires@aluno.senai.br>
2026-05-26 08:36:51 -0300

    corrigindo porta

--- musica-classica/src/server.js
+++ musica-classica/src/server.js
@@ -2,8 +2,6 @@
 require("dotenv").config();
 // Importa a aplicação Express configurada do arquivo app.js
 const app = require("./app");
-// Define a porta em que o servidor rodará
-const PORT = 3000;
 // Define a porta em que o servidor rodará, usando a variável de ambiente PORT ou 3000 como padrão
 const PORT = process.env.PORT || 3000;
 // Inicia o servidor e coloca-o em escuta na porta especificada
